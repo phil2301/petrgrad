bdad1360ef32101ce72fb741c51c30b14b7a5823
philip Hyltoft <philip Hyltoft>
2017-02-23 11:41:57 +0100

    add data navn, pris and rabatpris

--- script.js
+++ script.js
@@ -5,8 +5,6 @@ function sidenVises() {
 
     // læs produktliste
     $.getJSON("http://petlatkea.dk/2017/dui/api/productlist?callback=?", visProduktListe);
-
-    visProdukt();
 }
 
 function visProduktListe ( listen ) {
@@ -18,7 +16,13 @@ function visProdukt (produkt) {
     console.log(produkt);
     //Vis produkt_template
     var klon = document.querySelector("#produkt_template").content.cloneNode(true);
+
     // indsæt data i klon
+    klon.querySelector(".data_navn").innerHTML = produkt.navn;
+    klon.querySelector(".data_pris").innerHTML = produkt.pris;
+
+    var rabatpris = Math.ceil( produkt.pris - (produkt.pris*produkt.rabatsats/100 ) );
+    klon.querySelector(".data_rabatpris").innerHTML = rabatpris;
 
     // append klon til .produkt_liste
     document.querySelector(".produktliste").appendChild(klon);
