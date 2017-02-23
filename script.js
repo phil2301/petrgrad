7a4ac098956e264f8822daba6d6b1ea35c1c5b4b
philip Hyltoft <philip Hyltoft>
2017-02-23 11:32:05 +0100

    load JSON produktliste and create template clone for each

--- script.js
+++ script.js
@@ -2,10 +2,20 @@ window.addEventListener("load", sidenVises);
 
 function sidenVises() {
     console.log("Siden vises");
+
+    // læs produktliste
+    $.getJSON("http://petlatkea.dk/2017/dui/api/productlist?callback=?", visProduktListe);
+
     visProdukt();
 }
 
-function visProdukt () {
+function visProduktListe ( listen ) {
+    console.table(listen);
+    listen.forEach(visProdukt);
+}
+
+function visProdukt (produkt) {
+    console.log(produkt);
     //Vis produkt_template
     var klon = document.querySelector("#produkt_template").content.cloneNode(true);
     // indsæt data i klon
